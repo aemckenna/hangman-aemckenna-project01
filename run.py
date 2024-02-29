2024fe5ad5c98d8ca22e75d6f7852748c87021ff
aemckenna <mr.ashermckenna@gmail.com>
2024-02-29 10:05:13 -0500

    Run file update

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import game_manager as GM
 
 # ------SETTINGS------
 ai = False                                                                                                       # should the words be provided by a human or selected by the computer?
-difficulty = input("What level of difficulty? (Debug, Easy, Hard) ")                                            # if computer-generated, what difficulty level should it play at?
-word_locs = {"debug": "words/debug_dict.txt", "easy": "words/easy_dict.txt", "hard": "words/hard_dict.txt"}     # depending on the selection of difficulty, use the appropriate text file
+difficulty = input("What level of difficulty? (Debug, Easy, Hard) ")                                             # if computer-generated, what difficulty level should it play at?
+word_locs = {"debug": "words/debug_dict.txt", "easy": "words/easy_dict.txt", "hard": "words/hard_dict.txt"}      # depending on the selection of difficulty, use the appropriate text file
 words = word_locs[difficulty.lower()]
-num_guesses = 10                                                                                                # how many guesses should be the maximum?
+num_guesses = 10                                                                                                 # how many guesses should be the maximum?
 debug = False                                                                                                    # can modify this to see helpful information (such as how many words remain for a computer-generated word)
 
 # ------BEGIN GAME------
